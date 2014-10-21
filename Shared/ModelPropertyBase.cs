b2bd176e873f7989c6e3a20e8ea14ff559c33467
Micah Osborne <micahosborne@gmail.com>
2014-10-20 20:45:35 -0400

    RC 2

--- Shared/ModelPropertyBase.cs
+++ Shared/ModelPropertyBase.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using UniRx;
-using UnityEngine;
 
 public class P<T> : ISubject<T>, IObservableProperty, INotifyPropertyChanged
 {
