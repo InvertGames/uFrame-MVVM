4eef1332697edb967120d311f3d234b33c609dde
ZimM <fearcatalyst@gmail.com>
2015-02-24 04:25:36 +0200

    add ability to subscribe to commands with arguments

--- Base/Commands/CommandWith.cs
+++ Base/Commands/CommandWith.cs
@@ -268,3 +268,31 @@ public class CommandWithSenderAndArgument<TSender,TArgument> : CommandBase<TArgu
         return true;
     }
 }
+
+public static partial class ObservableExtensions
+{
+    public static IDisposable Subscribe<T>(this CommandBase<T> source)
+    {
+        return source.Subscribe(Observer.Create<T>(Stubs.Ignore<T>, Stubs.Throw, Stubs.Nop));
+    }
+
+    public static IDisposable Subscribe<T>(this CommandBase<T> source, Action<T> onNext)
+    {
+        return source.Subscribe(Observer.Create(onNext, Stubs.Throw, Stubs.Nop));
+    }
+
+    public static IDisposable Subscribe<T>(this CommandBase<T> source, Action<T> onNext, Action<Exception> onError)
+    {
+        return source.Subscribe(Observer.Create(onNext, onError, Stubs.Nop));
+    }
+
+    public static IDisposable Subscribe<T>(this CommandBase<T> source, Action<T> onNext, Action onCompleted)
+    {
+        return source.Subscribe(Observer.Create(onNext, Stubs.Throw, onCompleted));
+    }
+
+    public static IDisposable Subscribe<T>(this CommandBase<T> source, Action<T> onNext, Action<Exception> onError, Action onCompleted)
+    {
+        return source.Subscribe(Observer.Create(onNext, onError, onCompleted));
+    }
+}
